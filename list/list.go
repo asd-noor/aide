885638cfd3c835eaab519657813d43de243e8ca6
Asaduzzaman Noor <34137568+asd-noor@users.noreply.github.com>
2024-05-29 18:29:34 +0600

    Update list.go

--- list/list.go
+++ list/list.go
@@ -5,6 +5,8 @@ type applyFn[T any] func(T) T
 type predicateFn[T any] func(T) bool
 type foldFn[T any] func(T, T) T
 
+// Convert takes in a slice and transformer function, and
+// returns a different slice by applying the tranformer function on each element.
 func Convert[T any, U any](arr []T, fn transformFn[T, U]) (returnValue []U) {
 	for _, v := range arr {
 		returnValue = append(returnValue, fn(v))
@@ -13,6 +15,8 @@ func Convert[T any, U any](arr []T, fn transformFn[T, U]) (returnValue []U) {
 	return
 }
 
+// Map takes in a slice and a function to apply on every element of slice
+// and return the updated slice.
 func Map[T any](arr []T, fn applyFn[T]) (returnValue []T) {
 	for _, v := range arr {
 		returnValue = append(returnValue, fn(v))
@@ -21,6 +25,8 @@ func Map[T any](arr []T, fn applyFn[T]) (returnValue []T) {
 	return
 }
 
+// Filter takes in a slice and a predicate function to return a filtered slice
+// based on the condition in predicate function.
 func Filter[T any](arr []T, fn predicateFn[T]) (returnValue []T) {
 	for _, v := range arr {
 		if fn(v) {
@@ -31,6 +37,7 @@ func Filter[T any](arr []T, fn predicateFn[T]) (returnValue []T) {
 	return
 }
 
+// Fold reduces a slice to a single value based on given function to merge elements.
 func Fold[T any](arr []T, fn foldFn[T]) (returnValue T) {
 	arrLen := len(arr)
 	if arrLen == 0 {
