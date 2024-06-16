cdc2db9a1dfe48e4401bfc518b028d8fc03da2a8
Asaduzzaman Noor <asad.noor.antor@gmail.com>
2024-06-16 17:18:54 +0600

    flake update

--- flake.nix
+++ flake.nix
@@ -2,23 +2,22 @@
   description = "Go Functional Programming Package DevShell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }: 
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
         devShells.default = pkgs.mkShell {
-	  buildInputs = with pkgs; [
+	  hardeningDisable = [ "fortify" ];
+          buildInputs = with pkgs; [
 	    go_1_21
-	    # gitflow
 	  ];
-	};
+        };
       }
     );
 }
-
